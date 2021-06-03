753ab6025a8af0895fce2afd3c41aa642e1b7cca
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-03 13:29:26 -0500

    added some server routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+
+
+
 const App = function() {
   return (
     <div>Hello from App</div>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const colors = require('colors');
+const axios = require('axios');
 
-const githubToken = require('../config.js');
+const { GITHUB_TOKEN } = require('../config.js');
 
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea';
 
 let app = express();
 
@@ -12,6 +14,55 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.listen(3000);
 
-app.get('/', function(req, res) {
-  console.log(`GET request on /`.yellow)
-});
+app.get('/', ( (req, res) => {
+  console.log('GET request on /'.yellow)
+  })
+)
+
+app.get('/products/:productId', (async (req, res) => {
+  // console.log(req.params.productId);
+  console.log('GET request on /products'.yellow);
+  const productId = req.params.productId;
+  const options = {
+    method: 'GET',
+    url: `${API_URL}/products/${productId}`,
+    headers: {
+      Authorization: GITHUB_TOKEN,
+    },
+
+  };
+  const results = await axios(options).catch((err) => {
+    res.sendStatus(500);
+    console.log(err.response.data);
+  });
+  if (results) {
+    res.send(results.data);
+  }
+})
+
+);
+//        reviews
+app.get('/reviews/', ( async (req, res) => {
+  console.log('GET request on /reviews/'.yellow);
+  const options = {
+    method: 'GET',
+    url: `${API_URL}/reviews/`,
+    headers: {
+      Authorization: GITHUB_TOKEN,
+    },
+  };
+  const results = await axios(options).catch((err) => console.log(err.response.data));
+  res.send(results);
+}));
+
+//      /qa/questions
+app.get('/q&a', ( (req, res) => {
+
+})
+)
+//   /cart
+app.get('/cart', ( (req, res) => {
+
+})
+)
+// post interactios
