ae399fcfdac2a4e2c8a7b04c600c7a735a34cd23
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-07 15:59:44 -0500

    added basic carousel

--- client/components/overview/product/Product.jsx
+++ client/components/overview/product/Product.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Product({ styles }) {
+  let [index, setIndex] = useState(0);
+  function goLeft() {
+    if (index === 0) {
+      setIndex(styles.results.length - 1);
+    } else {
+      setIndex(index--);
+    }
+  }
+  function goRight() {
+    if (index === styles.results.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index++);
+    }
+  }
   return (
     <div>
-      <img src={styles.results[0].photos[0].url}></img>
+      <button onClick={goLeft}>left</button>
+      <img alt="hi" height="300" width="200" src={styles.results[index].photos[0].url} />
+      <button onClick={goRight}>right</button>
     </div>
   )
 }
