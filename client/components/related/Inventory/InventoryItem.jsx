bdda7e80d66f1e4f4b482977a0d463a5c92a728b
Jack Yang <zongchenisjack@gmail.com>
2021-06-16 10:12:37 -0700

    small fix on the border

--- client/components/related/Inventory/InventoryItem.jsx
+++ client/components/related/Inventory/InventoryItem.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Rating from '../Related/Rating';
 
 const InventoryItem = ({ item, deleteCH, className }) => (
-  <div className="item">
+  <div className={className ? 'itemWithFourth' : 'item'}>
     <img className="image" src={item.style.photos[0].url}></img>
     <i className="icon far fa-times-circle fa-1x" onClick={()=> deleteCH(item, item.style)} />
     <div className="category">{item.category}</div>
