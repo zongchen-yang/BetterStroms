934647b9ce0cd5aec11b18d8fb06e41c2565a9aa
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-17 17:15:03 -0500

    added some styling to description

--- client/components/overview/product/Description.jsx
+++ client/components/overview/product/Description.jsx
@@ -4,16 +4,20 @@ function Description({ product }) {
   return (
     <div id="descriptionContainer">
       <div id="description-text">
-        <span>{product.slogan}</span>
-        <span>{product.description}</span>
+        <div className="description-space-vertical">
+          <span id="slogan-text">{product.slogan}</span>
+        </div>
+        <div className="description-space-vertical">
+          <span className="small-text">{product.description}</span>
+        </div>
       </div>
-      <div id="descripton-right">
+      <div id="description-right">
         <div className="veritcalLine" />
         <div id="feature-text-container">
           {product.features.map((item, index) => (
             <div key={index} className="description-feature-text">
-              <span>{item.feature}</span>
-              <span>{item.value}</span>
+              <span className="feature-text">{item.feature}</span>
+              <span className="feature-text">{item.value}</span>
             </div>
           ))}
         </div>
