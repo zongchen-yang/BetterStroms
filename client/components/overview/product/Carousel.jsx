adf48ebced798dda1366c850096ef2bf3eb5e690
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-19 16:20:07 -0500

    moved expand image

--- client/components/overview/product/Carousel.jsx
+++ client/components/overview/product/Carousel.jsx
@@ -10,8 +10,17 @@ function Carousel({ style, photoIndex, clickHandler, expandedClicked }) {
   const [zoomX, setZoomX] = useState(0);
   const [zoomY, setZoomY] = useState(0);
 
+  const expandImageElement = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M396.795 396.8H320V448h128V320h-51.205zM396.8 115.205V192H448V64H320v51.205zM115.205 115.2H192V64H64v128h51.205zM115.2 396.795V320H64v128h128v-51.205z"/>
+    </svg>
+  );
+
   let mainImage;
   let imageSource;
+  let moveExpand = {
+    transform: '',
+  };
 
   if (!photoIndex) {
     imageSource = style.photos[0].url;
@@ -74,12 +83,18 @@ function Carousel({ style, photoIndex, clickHandler, expandedClicked }) {
     mainImage = <div id="main-image-container"><img id="mainImage" hidden={zoomed} alt="hi" src={imageSource} /></div>;
   }
 
+  if (expanded > 1) {
+    moveExpand = {
+      transform: 'translateX(450%)',
+    };
+  }
+
   useEffect(() => {
     if ((photoIndex === 0) || (expanded > 0)) {
       setLeftHidden(true);
     } else {
       setLeftHidden(false);
-    };
+    }
 
     if ((photoIndex === style.photos.length - 1) || expanded > 0) {
       setRightHidden(true);
@@ -90,12 +105,6 @@ function Carousel({ style, photoIndex, clickHandler, expandedClicked }) {
 
   const shevron = (
     <img src="assets/shevron_outlined.svg" alt="" width="64" height="64" />
-  );
-
-  const expandImageElement = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M396.795 396.8H320V448h128V320h-51.205zM396.8 115.205V192H448V64H320v51.205zM115.205 115.2H192V64H64v128h51.205zM115.2 396.795V320H64v128h128v-51.205z"/>
-    </svg>
   );
 
   return (
@@ -113,7 +122,7 @@ function Carousel({ style, photoIndex, clickHandler, expandedClicked }) {
           </button>
         </div>
       </div>
-      <div id="expand-image-container">
+      <div id="expand-image-container" style={moveExpand}>
         <button id="expand-image-button" type="button" onClick={expandImage}>
           {expandImageElement}
         </button>
