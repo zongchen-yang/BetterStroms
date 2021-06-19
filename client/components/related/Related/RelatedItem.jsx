199a74a2c3c507523b3353161c03670ed5a2a02b
Jack Yang <zongchenisjack@gmail.com>
2021-06-18 17:36:48 -0700

    css changes to compare section so it now handles overflow, hover on title now enables shadows, and other minor margin changes

--- client/components/related/Related/RelatedItem.jsx
+++ client/components/related/Related/RelatedItem.jsx
@@ -8,8 +8,8 @@ const RelatedItem = ({
     <img className="image" src={item.image} onClick={()=> displayItemCH(item.id)} />
     <i className="icon far fa-star fa-1x" onClick={() => {showCompareCH(item)}} />
     <div className="category">{item.category}</div>
-    <div className="name" onClick={()=>setId(item.id)}>{item.name}</div>
-    <div className="price">
+    <div className="relatedName" onClick={()=>displayItemCH(item.id)}>{item.name}</div>
+    <div className="relatedPrice">
       {item.sale ? <div className={"redPrice"}>${item.sale} </div> : null}
       <div className={item.sale ? "cross-out" : null}>${item.default_price}</div>
     </div>
